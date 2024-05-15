ec9891d224c3ea55be1c728c7fa89ab86821544c
Gabriel Spagnuolo <133052798+GabrielSpagnuolo15@users.noreply.github.com>
2024-05-14 20:08:08 -0400

    Iteration

---
+++ LeetCode_SQL_Questions/Question13.sql
@@ -0,0 +1,61 @@
+/*
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
+*/
+
+-- My Solution
+SELECT E1.name
+FROM Employee AS E1
+JOIN Employee AS E2 ON E1.id = E2.managerId
+GROUP BY E1.id, E1.name
+HAVING COUNT(*) >= 5;
+
+-- The Solution with Best Runtime
+SELECT e1.name
+FROM Employee e1
+JOIN Employee e2
+ON e1.id = e2.managerID
+GROUP BY e2.managerId
+HAVING COUNT(e2.managerID) >=5
